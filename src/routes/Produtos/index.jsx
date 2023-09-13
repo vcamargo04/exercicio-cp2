4f6af8843498523a3b8310430d56fd3fecde74d7
vcamargo04 <viniciusac2005@hotmail.com>
2023-09-13 09:47:27 -0300

    Adicionado  a lista de produtos na página produtos.

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+import { listaProdutos } from "../../componets/listaProdutos"
+
 export default function Produtos(){
     return(
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to ={`/produtos/editar/${prod.id }`}>
+                        Editar o Produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
     )
 }
