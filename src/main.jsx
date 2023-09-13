44583c00bbb9117bc54ab343cacde80e0a8e2d7b
vcamargo04 <viniciusac2005@hotmail.com>
2023-09-13 09:38:19 -0300

    Adicionado o caminho das páginas nas rotas.

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+    errorElement:<Error/>,
+    children:[
+      {
+
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "produtos/editar:id",
+        element: <EditarProdutos/>
+      },
+    ]
+  }
+]
+
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
